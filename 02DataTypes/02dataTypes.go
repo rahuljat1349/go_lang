9bf67e0f4deb01707425a47d34146f316ed59458
rahuljat1349 <rahuldudi1349@gmail.com>
2024-09-17 15:45:35 +0530

    update

---
+++ 02DataTypes/02dataTypes.go
@@ -0,0 +1,26 @@
+package datatypes
+
+import "fmt"
+
+func Run() {
+	// data Types
+
+	var name string = "Rahul dudi"
+	const PI = 3.14
+
+	fmt.Println(name)
+	fmt.Println(PI)
+
+	// All typical data types....
+
+	// ShortHand ->
+	person := "a person"
+	fmt.Println(person)
+
+	//Public variable (exported)
+	// var PublicVariable int = 42
+
+	//Private variable (unexported)
+	// var publicVariable int = 42
+}
+
